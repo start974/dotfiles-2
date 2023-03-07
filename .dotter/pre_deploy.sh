020f9d13ad6fbaf76c0b490730dbf65b1aebbb2f
jeremy.damour <jeremy.damour2@gmail.com>
2023-03-07 12:20:53 +0100

    update debian config

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -11,15 +11,18 @@ set -e
 
     {{pm_install}} {{#each pkgs }} "{{this}}" {{/each}}
 
-    {{#each cargo_pkgs }}
-        cargo install {{this}}
-    {{/each }}
 
+    cargo install {{#each cargo_pkgs }}{{this}}{{/each }}
+
+    pip install {{#each pip_pkgs }} {{this}} {{/each }}
+
+    sudo npm i -g {{#each npm_pkgs }} {{this}} {{/each }}
 
     {{#if not(is_executable omz)}}
         #omz update
         echo "install omyzsh"
         sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
     {{/if}}
 
 {{/if}}
--- zsh/config/completion.zsh
+++ zsh/config/completion.zsh
@@ -4,3 +4,5 @@ compdef _gnu_generic delta
 # lsd
 compdef _gnu_generic lsd
 
+# zsh-completions
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
