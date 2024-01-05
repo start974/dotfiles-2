29433e5d7dba14fde22658c95b5e464f4e43ac1c
jeremy.damour <jeremy.damour2@gmail.com>
2024-01-05 15:43:42 +0100

    add coq plugin for vim

--- nvim/lua/languages/coq.lua
+++ nvim/lua/languages/coq.lua
@@ -1,9 +1,21 @@
 return {
-  {
+  'tomtomjhj/vscoq.nvim',
+  dependencies = {
     'whonore/Coqtail',
-    --event = "BufferEnter *.v"
+    'neovim/nvim-lspconfig',
   },
-  {
-
+  init = function()
+    --vim.g.loaded_coqtail = 1
+    --vim.g.coqtail_supported = 0
+  end,
+  opts = {
+    vscoq = {
+      proof = {
+        mode = 1,
+      },
+    },
+    lsp = {
+      autostart = true,
+    },
   },
 }
