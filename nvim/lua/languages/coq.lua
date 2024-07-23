decf661b75e9432b89e979bc54d98ff532c51c74
jeremy.damour <jeremy.damour2@gmail.com>
2024-07-23 12:55:13 +0200

    add vim command coq restart

--- nvim/lua/languages/coq.lua
+++ nvim/lua/languages/coq.lua
@@ -1,66 +1,40 @@
 return {
   {
     'whonore/Coqtail',
+    init = function()
+      vim.cmd [[
+        g:coqtail_dune_compile_deps = 1
+      ]]
+    end,
     cond = function()
       return not vim.g.vscode
     end,
   },
-  --{
-  --'tomtomjhj/vscoq.nvim',
-  --dependencies = {
-  --'whonore/Coqtail',
-  --'neovim/nvim-lspconfig',
-  --},
-  --config = function()
-  --vim.g.coqtail_nomap = 1
-  --vim.g.loaded_coqtail = 1
-  --vim.g.coqtail_supported = 0
-
-  --require('vscoq').setup {}
-  ----vscoq = {
-  ----proof = {
-  ----mode = 0,
-  ------sticky = false,
-  ------workers = 4,
-  ----},
-  ----},
-  ------completion = {
-  ------enable = true,
-  ------},
-  ----lsp = {
-  ----autostart = true,
-  ----},
-  ----}
-  --end,
-
-  --keys = {
-  --{
-  --'<leader>cj',
-  --'<Cmd>VsCoq stepForward<CR>',
-  --mode = 'n',
-  --desc = '[Coq] Interpret Forward in proof',
-  --},
-  --{
-  --'<leader>ck',
-  --'<Cmd>VsCoq stepBackward<CR>',
-  --mode = 'n',
-  --desc = '[Coq] Interpret Backward in proof',
-  --},
-  --{
-  --'<leader>cG',
-  --'<Cmd>VsCoq interpretToEnd<CR>',
-  --mode = 'n',
-  --desc = '[Coq] Interpret to end',
-  --},
-  --{
-  --'<leader>cp',
-  --'<Cmd>VsCoq interpretToPoint<CR>',
-  --mode = 'n',
-  --desc = '[Coq] Interpret to point',
-  --},
-  --},
-  --cond = function()
-  --return not vim.g.vscode
-  --end,
-  --},
+  -- setup lspconfig
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        coq_lsp = {
+          cmd = 'coq-lsp',
+          filetypes = { 'coq' },
+          root_dir = function(fname)
+            local util = require 'lspconfig.util'
+            return util.root_pattern '_CoqProject' (fname) or util.find_git_ancestor(fname)
+          end,
+          single_file_support = true,
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>cR',
+        "<cmd>CoqStop<cr><cmd>CoqToLine<cr><cmd>LspRestart<cr>",
+        mode = 'n',
+        desc = 'Coq Restart',
+        noremap = true,
+      },
+    },
+  },
 }
--- nvim/lua/languages/latex.lua
+++ nvim/lua/languages/latex.lua
@@ -5,11 +5,6 @@ return {
       if type(opts.ensure_installed) == 'table' then
         vim.list_extend(opts.ensure_installed, { 'bibtex', 'latex' })
       end
-      --if type(opts.highlight.disable) == 'table' then
-        --vim.list_extend(opts.highlight.disable, { 'latex' })
-      --else
-        --opts.highlight.disable = { 'latex' }
-      --end
     end,
   },
   {
@@ -27,15 +22,6 @@ return {
       vim.g.vimtex_mappings_disable = { ['n'] = { 'K' } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable 'pplatex' == 1 and 'pplatex' or 'latexlog'
     end,
-  },
-  {
-    'folke/which-key.nvim',
-    optional = true,
-    opts = {
-      defaults = {
-        ['<localLeader>l'] = { name = '+vimtex' },
-      },
-    },
   },
   {
     'neovim/nvim-lspconfig',
